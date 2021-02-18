5f9f22dff6896a647426aa11662c89fd2847487e
Mohammed Manssour <manssour.mohammed@gmail.com>
2021-02-18 16:45:46 +0200

    renamed TypesGenerator to generator and added function to convert styling object to classnames string

---
+++ src/generator/index.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import minimist from 'minimist';
+import chalk from 'chalk';
+import ReactTailwindTypesGenerator from './ReactTailwindTypesGenerator';
+import TypeTemplatesCreator from './Adapters/TypeTemplatesCreator';
+
+const args = minimist(process.argv.slice(2));
+
+// find outdir
+const outdir = args.outdir || 'src';
+const verbose = args.verbose || false;
+
+// run styling object interface generator
+async function run() {
+  console.log(chalk.blue(`Generating TailwindStylingObject interface info "${path.resolve(outdir)}"....`));
+  await new ReactTailwindTypesGenerator(
+    new TypeTemplatesCreator(outdir),
+    verbose
+  ).run();
+  console.log(chalk.green(`✅ TailwindStylingObject interface was generated successfully`));
+}
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
-import path from 'path';
-import minimist from 'minimist';
-import chalk from 'chalk';
-import ReactTailwindTypesGenerator from './TypesGenerator/ReactTailwindTypesGenerator';
-import TypeTemplatesCreator from './TypesGenerator/Adapters/TypeTemplatesCreator';
+import flatMapDeep from 'lodash/flatMapDeep';
 
-const args = minimist(process.argv.slice(2));
+const flatten = (obj = {}) => {
+  if (typeof obj !== 'object') return [];
 
-// find outdir
-const outdir = args.outdir || 'src';
-const verbose = args.verbose || false;
+  const valuesArr = Object.entries(obj).map(item => {
+    if (Array.isArray(item[1]) || typeof item[1] === 'string') {
+      return item[1];
+    } else if (typeof item[1] === 'object') {
+      return flatten(item[1]);
+    }
+  });
 
-// run styling object interface generator
-async function run() {
-  console.log(chalk.blue(`Generating TailwindStylingObject interface info "${path.resolve(outdir)}"....`));
-  await new ReactTailwindTypesGenerator(
-    new TypeTemplatesCreator(outdir),
-    verbose
-  ).run();
-  console.log(chalk.green(`✅ TailwindStylingObject interface was generated successfully`));
+  return flatMapDeep(valuesArr) || [];
+};
+
+export function twStyle(props: any): string {
+  if (!props) return '';
+
+  return flatten(props).join(' ');
 }
-run();
